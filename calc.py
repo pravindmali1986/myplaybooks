02380019f109a888286f4a6c83e3beba7c433e6c
Pravin M <Pravin@localhost.localdomain>
2019-05-05 21:02:08 +0530

    New file

---
+++ calc.py
@@ -0,0 +1,17 @@
+# Program make a simple calculator that can add, subtract, multiply and divide using functions
+
+# This function adds two numbers 
+def add(x, y):
+   return x + y
+
+# This function subtracts two numbers 
+def subtract(x, y):
+   return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+   return x * y
+
+# This function divides two numbers
+def divide(x, y):
+   return x / y
